f9b66f59c2f79c9f8edc35fc80fc5ce80234b636
Talib Naseri <taleb.nasseri18@vit.edu>
2020-06-05 16:37:42 +0000

    feat: handle create agent transactions

---
+++ processor/simple_supply_tp/payload.py
@@ -0,0 +1,16 @@
+from simple_supply_protobuf import payload_pb2
+class SimpleSupplyPayload(object):
+    def __init__(self, payload):
+        self._transaction = payload_pb2.SimpleSupplyPayload()
+        self._transaction.ParseFromString(payload)
+    @property
+    def action(self):
+        return self._transaction.action
+    @property
+    def data(self):
+        if self._transaction.HasField('create_agent'):
+            return self._transaction.create_agent
+        return None
+    @property
+    def timestamp(self):
+        return self._transaction.timestamp
