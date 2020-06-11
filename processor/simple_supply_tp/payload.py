9e68acb303717f3683b495ea7110bbd5bbcab34f
Talib Naseri <taleb.nasseri18@vit.edu>
2020-06-11 11:44:57 +0000

    feat: validate payload action and field

--- processor/simple_supply_tp/payload.py
+++ processor/simple_supply_tp/payload.py
@@ -1,3 +1,4 @@
+from sawtooth_sdk.processor.exceptions import InvalidTransaction
 from simple_supply_protobuf import payload_pb2
 class SimpleSupplyPayload(object):
     def __init__(self, payload):
@@ -8,13 +9,19 @@ class SimpleSupplyPayload(object):
         return self._transaction.action
     @property
     def data(self):
-        if self._transaction.HasField('create_agent'):
+        if self._transaction.HasField('create_agent') and \
+            self._transaction.action == \
+                payload_pb2.SimpleSupplyPayload.CREATE_AGENT:
             return self._transaction.create_agent
-        if self._transaction.HasField('create_record'):
+        if self._transaction.HasField('create_record') and \
+            self._transaction.action == \
+                payload_pb2.SimpleSupplyPayload.CREATE_RECORD:
             return self._transaction.create_record
-        if self._transaction.HasField('transfer_record'):
+        if self._transaction.HasField('transfer_record') and \
+            self._transaction.action == \
+                payload_pb2.SimpleSupplyPayload.TRANSFER_RECORD:
             return self._transaction.transfer_record
-        return None
+        raise InvalidTransaction('Action does not match payload data')
     @property
     def timestamp(self):
         return self._transaction.timestamp
