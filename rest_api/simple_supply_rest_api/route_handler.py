5e55870302c5e60900ce417ce2ebeb961199a07b
Vedant Mahajan <vedantmahajan111@gmail.com>
2020-06-28 14:00:23 +0000

    chore: remove request reference

--- rest_api/simple_supply_rest_api/route_handler.py
+++ rest_api/simple_supply_rest_api/route_handler.py
@@ -57,7 +57,7 @@ class RouteHandler(object):
             'agent': agent
         }
         return json_response(response)
-    async def list_agents(self, request):
+    async def list_agents(self):
         agent_list = await self._database.fetch_all_agent_resources()
         return json_response(agent_list)
     async def fetch_agent(self, request):
@@ -80,7 +80,7 @@ class RouteHandler(object):
             timestamp=get_time())
         return json_response(
             {'data': 'Create record transaction submitted'})
-    async def list_records(self, request):
+    async def list_records(self):
         record_list = await self._database.fetch_all_record_resources()
         return json_response(record_list)
     async def fetch_record(self, request):
